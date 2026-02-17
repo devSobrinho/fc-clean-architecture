c45c99c2819bf734725a87e499abb3c04e83ab23
dev.sobrinho <dev.sobrinho@gmail.com>
2026-02-17 11:34:39 -0300

    feat(product): implement find product use case and related DTOs
    - Created InputFindProductDto and OutputFindProductDto interfaces in find.product.dto.ts for input and output data structure.
    - Implemented FindProductUseCase class in find.product.usecase.ts to handle the logic for finding a product by ID.
    - Added integration tests in find.product.integration.spec.ts to verify the find product use case functionality.
    - Developed unit tests in find.product.unit.spec.ts to ensure the find product use case behaves correctly with mocked data.
    
    These changes enhance the product management capabilities by allowing users to retrieve product details based on their ID, ensuring a more robust and testable codebase.

---
+++ src/usecase/product/find/find.product.integration.spec.ts
@@ -0,0 +1,62 @@
+import { Sequelize } from "sequelize-typescript";
+
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import FindProductUseCase from "./find.product.usecase";
+
+describe("Test find product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new FindProductUseCase(productRepository);
+
+    const product = ProductFactory.create("a", "Product 1", 10);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: product.id,
+    };
+
+    const output = {
+      id: product.id,
+      name: "Product 1",
+      price: 10,
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should not find a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new FindProductUseCase(productRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    expect(() => {
+      return usecase.execute(input);
+    }).rejects.toThrow("Product not found");
+  });
+});
